4e2545c9811f37f933a6528e61cf1a423829b80e
MatheusKerscher <kerschermatheus12@gmail.com>
2022-11-09 10:55:45 -0300

    Configuração do projeto

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Components
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonDeleteAllComponent } from './components/button-delete-all/button-delete-all.component';
+import { InputAddItensComponent } from './components/input-add-itens/input-add-itens.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+//Pages
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    ButtonDeleteAllComponent,
+    InputAddItensComponent,
+    TodoListComponent,
+    HomeComponent,
+  ],
+  imports: [CommonModule],
+})
+export class HomeModule {}
