342a6b4a4e82f2d13bd59952bc1dfe4b8bb164ab
MatheusKerscher <kerschermatheus12@gmail.com>
2022-11-11 10:59:39 -0300

    Criação do layout estilizado e começo da lógica

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  taskList: Array<TaskList> = [
+    {
+      task: 'Teste',
+      checked: false,
+    },
 
-  constructor() { }
+    {
+      task: 'Teste2',
+      checked: true,
+    },
+  ];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  deleteTask(event: number) {
+    this.taskList.splice(event, 1);
   }
 
+  deleteAllTask() {
+    const confim = window.confirm(
+      'Você realmente deseja excluir todas as suas tasks?'
+    );
+    if (confim) this.taskList = [];
+  }
 }
