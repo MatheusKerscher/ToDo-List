9ae13a3f9c938bbfb08627259273ec5fae943fad
MatheusKerscher <kerschermatheus12@gmail.com>
2022-11-17 11:57:35 -0300

    criação da comunicação e local storage

--- src/app/modules/home/components/input-add-itens/input-add-itens.component.ts
+++ src/app/modules/home/components/input-add-itens/input-add-itens.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-input-add-itens',
   templateUrl: './input-add-itens.component.html',
-  styleUrls: ['./input-add-itens.component.scss']
+  styleUrls: ['./input-add-itens.component.scss'],
 })
 export class InputAddItensComponent implements OnInit {
+  @Output() emitItemTaskList = new EventEmitter();
 
-  constructor() { }
+  addItemTaskList: string = '';
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  submitItemTaskList() {
+    this.addItemTaskList = this.addItemTaskList.trim();
+    if (this.addItemTaskList) {
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = '';
+    }
   }
-
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { TaskList } from '../../model/task-list';
 
 @Component({
@@ -6,22 +6,18 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent implements OnInit {
-  taskList: Array<TaskList> = [
-    {
-      task: 'Teste',
-      checked: false,
-    },
-
-    {
-      task: 'Teste2',
-      checked: true,
-    },
-  ];
+export class TodoListComponent implements OnInit, DoCheck {
+  taskList: Array<TaskList> = JSON.parse(
+    localStorage.getItem('taskList') || '[]'
+  );
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngDoCheck(): void {
+    this.setLocalStorage();
+  }
 
   deleteTask(event: number) {
     this.taskList.splice(event, 1);
@@ -32,5 +28,30 @@ export class TodoListComponent implements OnInit {
       'Você realmente deseja excluir todas as suas tasks?'
     );
     if (confim) this.taskList = [];
+  }
+
+  addItemTaskList(event: string) {
+    const task: TaskList = { checked: false, task: event };
+    this.taskList.push(task);
+  }
+
+  validationInput(event: string, index: number) {
+    if (!event.length) {
+      const confirm = window.confirm('Task está vazia, deseja deletar?');
+
+      if (confirm) {
+        this.deleteTask(index);
+      }
+    }
+  }
+
+  setLocalStorage() {
+    if (this.taskList) {
+      this.taskList.sort(
+        (first, last) => Number(first.checked) - Number(last.checked)
+      );
+
+      localStorage.setItem('taskList', JSON.stringify(this.taskList));
+    }
   }
 }
